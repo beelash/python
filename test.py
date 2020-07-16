86f31fb71f6684a4f4d255f2be36bcfb9b16a90f
Vasyl Nastiuk <vasil.beelash@gmail.com>
2020-07-16 12:44:04 +0300

    test file

---
+++ test.py
@@ -0,0 +1,18 @@
+#name = input ("what is your name? ")
+#print ("hi,",name)
+
+#num1 = int (input ("первая цыфра: "))
+#num2 = float (input ("вторая цыфра: "))
+#res = num1 + num2
+#print ("is:",res)
+
+#интересный синтаксис условия
+num = int (input ("цыфра: "))
+A = "yes" if num == 5 else "no"
+print ("is:",A)
+
+#i = 0
+#a = int (input ("цыфра: "))
+#while i<=a:
+#	print (i)
+#	i += 1

7254be26da1533c7351cc656039b8a7326ac5091
Vasyl Nastiuk <vasil.beelash@gmail.com>
2020-07-16 16:16:35 +0300

    import

---
+++ module.py
@@ -0,0 +1,4 @@
+def hi():
+    print('hello')
+def add (x,y):
+    return x+y
--- test.py
+++ test.py
@@ -11,10 +11,47 @@
 ##A = "yes" if num == 5 else "no"
 #print ("is:",A)
 
- 
+#i = 0
+#a = int (input ("number: "))
+#while i<=a:
+#	print (i)
+#	i += 1
 
-i = 0
-a = int (input ("number: "))
-while i<=a:
-	print (i)
-	i += 1
+#f = open('txt.txt')
+#print(f.read(5))
+#f.close
+
+#f = open('txt.txt','r')
+#for line in f:
+#   print(line)
+#f.close
+
+#f = open('txt.txt','w')
+#f.write ('Nastiuk Vasyl\n0950975820\nvasil.nastiuk@gmail.com')
+#f.close
+
+#with open('txt.txt','wt', encoding='utf-8') as inFile:
+#	num = int(input())
+#	line = str('1/' + str(num) + '=' + str(1/num))
+#	print(line)
+#	inFile.write(line)
+
+#import math
+#print (math.pi)
+
+#import time
+#import os
+#print(time.time())
+#print(os.getcwd())
+
+#случайное число
+#import random as r
+#print(int(r.random()*100))
+
+#import module as m
+#m.hi()
+#print(m.add(int('45'),int('34')))
+
+#from module import hi,add
+#hi()
+#print(add('sta','rt'))
